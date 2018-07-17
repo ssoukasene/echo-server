dbcc22eaa2cebe102448b31dd647eada3510dbe4
Stephen <ssoukasene@gmail.com>
2018-07-16 18:37:10 -0700

    Started echo client.

--- echo_client.py
+++ echo_client.py
@@ -1,15 +1,20 @@
 import socket
 import sys
-
+import traceback
 
 def client(msg, log_buffer=sys.stderr):
     server_address = ('localhost', 10000)
     # TODO: Replace the following line with your code which will instantiate
     #       a TCP socket with IPv4 Addressing, call the socket you make 'sock'
-    sock = None
+    #sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP)
+    sock = socket.socket()
+    # cut and pasted above from the demo file, but these are the default options
+    # so you dont' have to use any arguments
     print('connecting to {0} port {1}'.format(*server_address), file=log_buffer)
+    
     # TODO: connect your socket to the server here.
-
+    sock.connect(server_address)
+    
     # you can use this variable to accumulate the entire message received back
     # from the server
     received_message = ''
@@ -20,6 +25,8 @@ def client(msg, log_buffer=sys.stderr):
         print('sending "{0}"'.format(msg), file=log_buffer)
         # TODO: send your message to the server here.
 
+        sock.sendall(msg.encode()) # instead of sock.sendall(msg) because msg is a character string not a byte string
+
         # TODO: the server should be sending you back your message as a series
         #       of 16-byte chunks. Accumulate the chunks you get to build the
         #       entire reply from the server. Make sure that you have received
@@ -27,16 +34,30 @@ def client(msg, log_buffer=sys.stderr):
         #
         #       Log each chunk you receive.  Use the print statement below to
         #       do it. This will help in debugging problems
-        chunk = ''
-        print('received "{0}"'.format(chunk.decode('utf8')), file=log_buffer)
+        
+        while True:
+            chunk = sock.recv(16) # it can help with performance to help make the chunk of your memory
+            # fit inside a page (which is 4Kb or 4096 bytes)
+            receive_message += chunk.decode() # chunk is in bytes so we need to decode it into character string
+            print('received "{0}"'.format(chunk.decode('utf8')), file=log_buffer)
+
+            if len(receive_message) >= len(msg):
+                break
+    except Exception as e:
+        print(e)
+        traceback.print_exc()
+        sys.exit(1)
+    
     finally:
         # TODO: after you break out of the loop receiving echoed chunks from
         #       the server you will want to close your client socket.
         print('closing socket', file=log_buffer)
-
+        sock.close()
+        
         # TODO: when all is said and done, you should return the entire reply
         # you received from the server as the return value of this function.
-
+        
+        return receive_message
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
